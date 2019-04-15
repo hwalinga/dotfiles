d15c341fd2573047ff0de80c277f76df37737515
Hielke Walinga <hielkewalinga@gmail.com>
2019-04-15 11:06:08 +0200

    Add urxvt plugins

--- make.bash
+++ make.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 files=(".tmux.conf" ".zshrc" ".bashrc-user" ".vimrc" ".bash_aliases"
-    ".config/nvim/init.vim" ".bashrc" ".Xdefaults)
+    ".config/nvim/init.vim" ".bashrc" ".Xdefaults")
 folders=(.vim/{backupfiles,colors,pack,swapfiles,undodir,pack/git-plugins/start}
-         .oh-my-zsh/custom/themes .tmux-panes)
+         .oh-my-zsh/custom/themes .tmux-panes .urxvt/ext repos old_dotfiles)
+urxvt_plugins=(
+    https://raw.githubusercontent.com/effigies/urxvt-perl/master/fullscreen
+    https://raw.githubusercontent.com/majutsushi/urxvt-font-size/master/font-size
+)
 
 for folder in "${folders[@]}"; do
     mkdir -p ~/$folder
 done
 
-mkdir ~/old_dotfiles
 for file in "${files[@]}"; do
     mkdir -p {file%/*}
     # file exists and is not a symbolic link.
@@ -19,24 +22,18 @@ for file in "${files[@]}"; do
     ln -s ~/dotfiles/${file##*/} ~/$file
 done;
 
-
-
 if [[ "$1" == "install-git" ]]; then
-    mkdir ~/git-tmp && cd $_
 
-    # vim
-    # Poor-man's vim plugin installer
-    git clone https://github.com/NLKNguyen/papercolor-theme
-    ln -s $PWD/papercolor-theme/colors/PaperColor.vim ~/.vim/colors
-    git clone https://github.com/w0rp/ale.git ~/.vim/pack/git-plugins/start/ale
-
-    # zsh
+    echo zsh
+    cd ~/repos
     sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
     git clone https://github.com/sobolevn/sobole-zsh-theme
     ln -s $PWD/sobole-zsh-theme/sobole.zsh-theme ~/.oh-my-zsh/custom/themes/sobole.zsh-theme
     echo 'Manually change terminal with `chsh -s `which zsh``'
 
-    # This probably breaks
-    git clone https://github.com/hwalinga/gnome-terminal-colors
-    echo "Manually set up your terminal color schemes"
+    echo urxvt
+    cd ~/.urxvt/ext
+    for i in "${urxvt_plugins[@]}"; do
+        wget $i
+    done
 fi
