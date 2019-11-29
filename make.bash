0037ded49e3f349c77563c1388d91109f972ad92
Hielke Walinga <hielkewalinga@gmail.com>
2019-11-29 16:38:59 +0100

    Fix something in make.bash

--- make.bash
+++ make.bash
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-files=(".tmux.conf" ".zshrc" ".bashrc-user" ".vimrc" ".bash_aliases"
-    ".config/nvim/init.vim" ".bashrc" ".Xdefaults")
-folders=(.vim/{backupfiles,colors,pack,swapfiles,undodir,pack/git-plugins/start}
-         .oh-my-zsh/custom/themes .tmux-panes .urxvt/ext repos old_dotfiles)
+check_installed () {
+    if [[ `apt list --installed | grep $1` ]]; then
+        echo "$1 is not installed"
+        exit
+    fi
+}
+
+check_installed zsh
+check_installed rxvt
+
+files=(
+    ".tmux.conf" ".zshrc" ".bashrc-user" ".vimrc" ".bash_aliases"
+    ".config/nvim/init.vim" ".bashrc" ".Xdefaults"
+)
+folders=(
+    .vim/{backupfiles,colors,pack,swapfiles,undodir,pack/git-plugins/start}
+     .oh-my-zsh/custom/themes .tmux-panes .urxvt/ext repos old_dotfiles
+)
 urxvt_plugins=(
     https://raw.githubusercontent.com/effigies/urxvt-perl/master/fullscreen
     https://raw.githubusercontent.com/majutsushi/urxvt-font-size/master/font-size
 )
+zsh_plugins=(
 
+)
+
+echo "Make folders."
 for folder in "${folders[@]}"; do
     mkdir -p ~/$folder
 done
 
+echo "Symlink config files."
 for file in "${files[@]}"; do
-    mkdir -p {file%/*}
+    mkdir -p "${file%/*}"
     # file exists and is not a symbolic link.
     if [[ ! -h "$HOME/$file" && -f "$HOME/$file" ]]; then
         mv ~/$file ~/old_dotfiles
@@ -22,18 +41,15 @@ for file in "${files[@]}"; do
     ln -s ~/dotfiles/${file##*/} ~/$file
 done;
 
-if [[ "$1" == "install-git" ]]; then
+echo "Configure zsh."
+cd ~/repos
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone https://github.com/sobolevn/sobole-zsh-theme
+ln -s $PWD/sobole-zsh-theme/sobole.zsh-theme ~/.oh-my-zsh/custom/themes/sobole.zsh-theme
+echo 'Manually change terminal with `chsh -s `which zsh``'
 
-    echo zsh
-    cd ~/repos
-    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-    git clone https://github.com/sobolevn/sobole-zsh-theme
-    ln -s $PWD/sobole-zsh-theme/sobole.zsh-theme ~/.oh-my-zsh/custom/themes/sobole.zsh-theme
-    echo 'Manually change terminal with `chsh -s `which zsh``'
-
-    echo urxvt
-    cd ~/.urxvt/ext
-    for i in "${urxvt_plugins[@]}"; do
-        wget $i
-    done
-fi
+echo "Configure urxvt"
+cd ~/.urxvt/ext
+for i in "${urxvt_plugins[@]}"; do
+    wget $i
+done
